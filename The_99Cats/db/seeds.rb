1489cb85b1334d47c08bffe4244a573eb53d49b3
Mackinjosh <mackzumarraga@gmail.com>
2021-11-12 15:46:33 -0500

    Created partials and edit for edit and new

--- The_99Cats/db/seeds.rb
+++ The_99Cats/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cat.destroy_all
+Cat.create(name: "Penny", birth_date: "2007/06/01", color: "black", sex: 'F', description: 'Cutie pie!')
+Cat.create(name: "Lara", birth_date: "2020/09/01", color: "grey", sex: 'F', description: 'Tiny!')
